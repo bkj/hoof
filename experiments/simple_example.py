3c3e09db345917c4e87e53d18b1b418c9c31e0c0
Ben Johnson <bkj.322@gmail.com>
2019-03-28 13:23:19 -0400

    refactoring

--- experiments/simple_example.py
+++ experiments/simple_example.py
@@ -29,9 +29,9 @@ def mse(act, pred):
     return float(((act - pred) ** 2).mean())
 
 
-def train(model, opt, dataset, batch_size=10, support_size=5, query_size=5, num_batches=100):
+def train(model, opt, dataset, batch_size=10, support_size=5, query_size=5, num_samples=100):
     hist = []
-    gen = trange(num_batches // batch_size)
+    gen = trange(num_samples // batch_size)
     for batch_idx in gen:
         x_support, y_support, x_query, y_query, _ = dataset.sample_batch(
             batch_size=batch_size,
@@ -55,9 +55,9 @@ def train(model, opt, dataset, batch_size=10, support_size=5, query_size=5, num_
     return hist
 
 
-def valid(model, dataset, batch_size=10, support_size=5, query_size=5, num_batches=100):
+def valid(model, dataset, batch_size=10, support_size=5, query_size=5, num_samples=100):
     hist = []
-    gen = trange(num_batches // batch_size)
+    gen = trange(num_samples // batch_size)
     for batch_idx in gen:
         x_support, y_support, x_query, y_query, _ = dataset.sample_batch(
             batch_size=batch_size,
@@ -80,34 +80,30 @@ def valid(model, dataset, batch_size=10, support_size=5, query_size=5, num_batch
 # --
 # Dataset
 
-dataset = 'quadratic'
+dataset = 'sinusoid'
 popsize = None
 
-# datasets = {
-#     "sinusoid"  : SinusoidDataset,
-#     "power"     : PowerDataset,
-#     "quadratic" : QuadraticDataset,
-# }
+datasets = {
+    "sinusoid"  : SinusoidDataset,
+    "power"     : PowerDataset,
+    "quadratic" : QuadraticDataset,
+}
 
-# dataset_cls = datasets[dataset]
+dataset_cls = datasets[dataset]
 
-# train_dataset = dataset_cls(popsize=popsize)
-# valid_dataset = dataset_cls()
-
-train_dataset = QuadraticDataset(x_dim=3, popsize=popsize)
-valid_dataset = QuadraticDataset(x_dim=3)
+train_dataset = dataset_cls(popsize=popsize)
+valid_dataset = dataset_cls()
 
 # --
 # Train
 
-model = ALPACA(x_dim=3, y_dim=1, sig_eps=0.01, hidden_dim=128, final_dim=128, activation='tanh')
-model = model.cuda()
+model = ALPACA(input_dim=1, output_dim=1, sig_eps=0.01, hidden_dim=128, activation='tanh').cuda()
 
 train_history = []
 lrs = [1e-4, 1e-5]
 opt = torch.optim.Adam(model.parameters(), lr=lrs[0])
 
-train_kwargs = {"batch_size" : 128, "support_size" : 10, "query_size" : 10, "num_batches" : 30000}
+train_kwargs = {"batch_size" : 128, "support_size" : 10, "query_size" : 10, "num_samples" : 30000}
 
 for lr in lrs:
     for p in opt.param_groups:
@@ -130,18 +126,61 @@ print('final_valid_loss=%f' % np.mean(valid_history[-100:]), file=sys.stderr)
 # --
 # Plot example
 
-x_s, y_s, _, _, fn = valid_dataset.sample_one(support_size=10, query_size=0)
-x_grid = np.linspace(*valid_dataset.x_range, 1000)
+# umodel = ALPACA(input_dim=1, output_dim=1, sig_eps=0.01, hidden_dim=128, activation='tanh').cuda()
+# uopt = torch.optim.Adam(umodel.parameters(), lr=lrs[0])
+# train(umodel, uopt, train_dataset, batch_size=128, support_size=10, query_size=10, num_samples=128000)
+
+def rbf_pred(x_s, y_s, x_grid, **kwargs):
+    rbf = RBFSampler(**kwargs)
+    p_s = rbf.fit_transform(x_s)
+    lr  = LinearRegression().fit(p_s, y_s)
+    return lr.predict(rbf.transform(x_grid))
+
+
+x_s, y_s, _, _, fn = valid_dataset.sample_one(support_size=5, query_size=0)
+x_grid = np.linspace(*valid_dataset.x_range, 1000).reshape(-1, 1)
 y_grid = fn(x_grid)
 
-inp = list2tensors((x_s, y_s, x_grid[...,None]), cuda=model.is_cuda)
+inp = list2tensors((x_s, y_s, x_grid), cuda=model.is_cuda)
 mu, sig, _ = model(*inp)
 mu, sig = tensors2list((mu, sig), squeeze=True)
 
+# umu, usig, _ = umodel(*inp)
+# umu, usig = tensors2list((umu, usig), squeeze=True)
+
 _ = plt.scatter(x_s, y_s, c='black')
 _ = plt.plot(x_grid, y_grid, c='black')
-_ = plt.plot(x_grid, mu)
-_ = plt.fill_between(x_grid, mu - 1.96 * np.sqrt(sig), mu + 1.96 * np.sqrt(sig), alpha=0.2)
+_ = plt.plot(x_grid, mu, alpha=0.75)
+# _ = plt.plot(x_grid, umu, alpha=0.75)
+# _ = plt.plot(x_grid, rbf_pred(x_s, y_s, x_grid, n_components=50, gamma=0.25), c='orange', alpha=0.75)
+# _ = plt.plot(x_grid, rbf_pred(x_s, y_s, x_grid, n_components=50, gamma=0.5), c='orange', alpha=0.75)
+# _ = plt.plot(x_grid, rbf_pred(x_s, y_s, x_grid, n_components=50, gamma=1.0), c='orange', alpha=0.75)
+_ = plt.fill_between(x_grid.squeeze(), mu - 1.96 * np.sqrt(sig), mu + 1.96 * np.sqrt(sig), alpha=0.2)
+# _ = plt.fill_between(x_grid.squeeze(), umu - 1.96 * np.sqrt(sig), umu + 1.96 * np.sqrt(sig), alpha=0.2)
 _ = plt.xlim(*valid_dataset.x_range)
 show_plot()
+
+# >>
+# RKS implementation
+from hoof.models import BLR
+from sklearn.kernel_approximation import RBFSampler
+from sklearn.linear_model import LinearRegression
+
+x_s, y_s, _, _, fn = valid_dataset.sample_one(support_size=10, query_size=0)
+x_grid = np.linspace(*valid_dataset.x_range, 1000).reshape(-1, 1)
+y_grid = fn(x_grid)
+
+
+
+
+_ = plt.scatter(x_s, y_s, c='black')
+_ = plt.plot(x_grid, y_grid, c='black', alpha=0.75)
+_ = plt.plot(x_grid, y_pred + np.random.uniform(0, 0.1, y_grid.shape), c='red', alpha=0.75)
+show_plot()
+
+
+# <<
+
+
+
 
--- hoof/models.py
+++ hoof/models.py
@@ -14,21 +14,90 @@ import numpy as np
 import torch
 from torch import nn
 
-class ALPACA(nn.Module):
-    # !! ignoring `f_nom` from the original
-    def __init__(self, x_dim, y_dim, sig_eps, num=1, activation='tanh', hidden_dim=128, final_dim=128):
+# --
+# Bayesian Linear Regression
+
+class BLR(nn.Module):
+    def __init__(self, sig_eps, input_dim, output_dim):
         super().__init__()
         
-        self.sig_eps  = nn.Parameter(torch.Tensor([sig_eps])) # BKJ: made this trainable
-        self.register_buffer('eye', torch.eye(y_dim))
+        # !! Should sig_eps be traininable?
+        # !! Notices that not training K or L_asym doesn't make a big difference
+        # !! Is the bias a good idea?
         
-        # Seems to work OK even if these are not trainable (these are just the priors, so makes sense)
-        self.K      = nn.Parameter(torch.zeros(final_dim, y_dim))
-        self.L_asym = nn.Parameter(torch.randn(final_dim, final_dim))
-        self.bias   = nn.Parameter(torch.zeros([1])) # BKJ: added this
+        self.sig_eps     = sig_eps
+        self.log_sig_eps = np.log(sig_eps)
+        self.register_buffer('sig_eps_eye', torch.eye(output_dim) * self.sig_eps)
+        
+        self.K      = nn.Parameter(torch.zeros(input_dim, output_dim))
+        self.L_asym = nn.Parameter(torch.randn(input_dim, input_dim))
+        self.bias   = nn.Parameter(torch.zeros([1]))
         
         torch.nn.init.xavier_uniform_(self.K)
         torch.nn.init.xavier_uniform_(self.L_asym)
+        
+        self.input_dim  = input_dim
+        self.output_dim = output_dim
+    
+    def forward(self, phi_support, y_support, phi_query, y_query=None):
+        L = self.L_asym @ self.L_asym.t()
+        
+        nobs = phi_support.shape[1]
+        if nobs > 0:
+            posterior_L     = (phi_support.transpose(1, 2) @ phi_support) + L[None,:]
+            posterior_L_inv = torch.inverse(posterior_L)
+            posterior_K     = posterior_L_inv @ ((phi_support.transpose(1, 2) @ y_support) + (L @ self.K))
+        else:
+            posterior_L_inv = torch.inverse(L)
+            posterior_K     = self.K # !! According to original code
+        
+        mu_pred = phi_query @ posterior_K + self.bias
+        
+        spread_fac = 1 + self._batch_quadform1(posterior_L_inv, phi_query)
+        
+        # Expand each element of spread_fac to y_dim diagonal matrix
+        sig_pred = torch.einsum('...i,jk->...ijk', spread_fac, self.sig_eps_eye)
+        
+        predictive_nll = None
+        if y_query is not None:
+            quadf = self._batch_quadform2(torch.inverse(sig_pred), y_query - mu_pred)
+            predictive_nll = self._sig_pred_logdet(spread_fac).mean() + quadf.mean()
+        
+        return mu_pred, sig_pred, predictive_nll
+    
+    def _batch_quadform1(self, A, b):
+        # Eq 8 helper
+        #   Also equivalent to: ((b @ A) * b).sum(dim=-1)
+        return torch.einsum('...ij,...jk,...ik->...i', b, A, b)
+    
+    def _batch_quadform2(self, A, b):
+        # Eq 10 helper
+        return torch.einsum('...i,...ij,...j->...', b, A, b)
+    
+    def _sig_pred_logdet(self, spread_fac):
+        # Compute logdet(sig_pred)
+        # Equivalent to [[ss.logdet() for ss in s] for s in sig_pred]
+        return self.output_dim * (spread_fac.log() + self.log_sig_eps)
+
+# --
+# ALPACA
+
+def _check_shapes(x_support, y_support, x_query, y_query, input_dim, output_dim):
+    assert x_support.shape[-1] == input_dim
+    assert y_support.shape[-1] == output_dim
+    assert x_query.shape[-1] == input_dim
+    if y_query is not None:
+        assert y_query.shape[-1] == output_dim
+    
+    if len(x_support.shape) == 2: x_support = x_support[None,:]
+    if len(y_support.shape) == 2: y_support = y_support[None,:]
+    if len(x_query.shape) == 2:   x_query = x_query[None,:]
+    
+    return x_support, y_support, x_query, y_query
+
+class ALPACA(nn.Module):
+    def __init__(self, input_dim, output_dim, sig_eps, num=1, activation='tanh', hidden_dim=128):
+        super().__init__()
         
         if activation == 'tanh':
             _act = nn.Tanh
@@ -38,175 +107,31 @@ class ALPACA(nn.Module):
             raise Exception('!! unknown activation %s' % activation)
         
         self.backbone = nn.Sequential(
-            nn.Linear(x_dim, hidden_dim),
+            nn.Linear(input_dim, hidden_dim),
             _act(),
             nn.Linear(hidden_dim, hidden_dim),
             _act(),
-            nn.Linear(hidden_dim, final_dim),
+            nn.Linear(hidden_dim, hidden_dim),
             _act() # Do we want this?
         )
         
-        self.num   = num
-        self.x_dim = x_dim
-        self.y_dim = y_dim
+        self.blr = BLR(sig_eps=sig_eps, input_dim=hidden_dim, output_dim=output_dim)
+        
+        self.num        = num
+        self.input_dim  = input_dim
+        self.output_dim = output_dim
     
     @property
     def is_cuda(self):
         return next(self.parameters()).is_cuda
     
     def forward(self, x_support, y_support, x_query, y_query=None):
-        # !! BUG: should be normalizing x_support, y_support, x, and y
-        assert x_support.shape[-1] == self.x_dim
-        assert y_support.shape[-1] == self.y_dim
-        assert x_query.shape[-1] == self.x_dim
-        if y_query is not None:
-            assert y_query.shape[-1] == self.y_dim
+        # !! POTENTIAL BUG: should be normalizing x_support, y_support, x, and y
         
-        if len(x_support.shape) == 2: x_support = x_support[None,:]
-        if len(y_support.shape) == 2: y_support = y_support[None,:]
-        if len(x_query.shape) == 2:   x_query = x_query[None,:]
+        x_support, y_support, x_query, y_query =\
+            _check_shapes(x_support, y_support, x_query, y_query, self.input_dim, self.output_dim)
         
-        L = torch.mm(self.L_asym, self.L_asym.t())
+        phi_support = self.backbone(x_support)
+        phi_query   = self.backbone(x_query)
         
-        nobs = x_support.shape[1]
-        if nobs > 0:
-            phi_support = self.backbone(x_support)
-            
-            posterior_L_inv = torch.bmm(phi_support.transpose(1, 2), phi_support)
-            posterior_L_inv = posterior_L_inv + L.unsqueeze(0)
-            posterior_L_inv = self.batch_inv(posterior_L_inv)
-            
-            posterior_K = torch.bmm(phi_support.transpose(1, 2), y_support) + torch.mm(L, self.K)
-            posterior_K = torch.bmm(posterior_L_inv, posterior_K)
-        else:
-            posterior_L_inv = self.batch_inv(L)
-            posterior_K     = self.K
-        
-        phi     = self.backbone(x_query)
-        mu_pred = torch.bmm(phi, posterior_K) + self.bias
-        
-        spread_fac = 1 + self.batch_quadform(posterior_L_inv, phi)
-        sig_pred = spread_fac.unsqueeze(-1) * self.eye.view(1, 1, self.y_dim, self.y_dim) * self.sig_eps
-        
-        predictive_nll = None
-        if y_query is not None:
-            logdet = self.y_dim * spread_fac.log() + (self.eye * self.sig_eps).logdet()
-            quadf  = self.batch_quadform2(self.batch_inv(sig_pred), y_query - mu_pred)
-            predictive_nll = (logdet + quadf).mean()
-        
-        return mu_pred, sig_pred, predictive_nll
-    
-    def batch_inv(self, x):
-        eye = x.new_ones(x.size(-1)).diag().expand_as(x)
-        x_inv, _ = torch.gesv(eye, x)
-        return x_inv
-    
-    def batch_quadform(self, A, b):
-        bs, hor, hdim = b.shape
-        
-        b_vec = b.unsqueeze(-1)
-        Ab = torch.bmm(A, b.transpose(1, 2)).transpose(1, 2).unsqueeze(-1)
-        
-        b_vec = b_vec.reshape(bs * hor, hdim, 1)
-        Ab    = Ab.reshape(bs * hor, hdim, 1)
-        out   = torch.bmm(b_vec.transpose(1, 2), Ab).squeeze(-1)
-        out   = out.reshape(bs, hor, 1)
-        return out
-    
-    def batch_quadform2(self, A, b):
-        bs, hor = A.shape[0], A.shape[1]
-        
-        b_vec   = b.unsqueeze(-1)
-        b_vec_t = b_vec.transpose(2, 3)
-        
-        A       = A.reshape(A.shape[0] * A.shape[1], A.shape[2], A.shape[3])
-        b_vec   = b_vec.reshape(b_vec.shape[0] * b_vec.shape[1], b_vec.shape[2], b_vec.shape[3])
-        b_vec_t = b_vec_t.reshape(b_vec_t.shape[0] * b_vec_t.shape[1], b_vec_t.shape[2], b_vec_t.shape[3])
-        
-        out = torch.bmm(b_vec_t, A)
-        out = torch.bmm(out, b_vec)
-        out = out.reshape(bs, hor, 1)
-        return out
-
-# # --
-# # Simplified version of the above
-# # Has not been tested to guarantee same results as original version
-
-# class ALPACA2(nn.Module):
-#     # !! ignoring `f_nom` from the original
-#     def __init__(self, x_dim, y_dim, sig_eps, activation='Tanh', hidden_dim=128, final_dim=128):
-#         super().__init__()
-        
-#         self.sig_eps     = sig_eps
-#         self.log_sig_eps = np.log(sig_eps)
-#         self.sig_eps_eye = torch.eye(self.y_dim) * self.sig_eps
-        
-#         # Seems to work OK even if this is not trainable
-#         self.K      = nn.Parameter(torch.zeros(final_dim, y_dim))     # last layer size
-#         self.L_asym = nn.Parameter(torch.randn(final_dim, final_dim)) # last layer size
-        
-#         torch.nn.init.xavier_uniform_(self.K)
-#         torch.nn.init.xavier_uniform_(self.L_asym)
-        
-#         _act = getattr(nn, activation)
-#         self.backbone = nn.Sequential(
-#             nn.Linear(x_dim, hidden_dim),
-#             _act(),
-#             nn.Linear(hidden_dim, hidden_dim),
-#             _act(),
-#             nn.Linear(hidden_dim, final_dim),
-#             _act(), # Do we want this?
-#         )
-        
-#         self.x_dim = x_dim
-#         self.y_dim = y_dim
-    
-#     def forward(self, x_support, y_support, x, y=None):
-#         assert x_support.shape[-1] == self.x_dim
-#         assert y_support.shape[-1] == self.y_dim
-#         assert x.shape[-1] == self.x_dim
-#         if y is not None:
-#             assert y.shape[-1] == self.y_dim
-        
-#         L = self.L_asym @ self.L_asym.t()
-        
-#         nobs = x_support.shape[1]
-#         if nobs > 0:
-#             phi_support = self.backbone(x_support)
-            
-#             posterior_L_inv = torch.inverse((phi_support.transpose(1, 2) @ phi_support) + L[None,:])
-#             posterior_K     = posterior_L_inv @ ((phi_support.transpose(1, 2) @ y_support) + (L @ self.K))
-#         else:
-#             posterior_L_inv = torch.inverse(L)
-#             posterior_K     = self.K # !! According to original code
-            
-        
-#         phi = self.backbone(x)
-#         mu_pred = phi @ posterior_K
-        
-#         spread_fac = 1 + self._batch_quadform1(posterior_L_inv, phi)
-        
-#         # Expand each element of spread_fac to y_dim diagonal matrix
-#         sig_pred = torch.einsum('...i,jk->...ijk', spread_fac, self.sig_eps_eye)
-        
-#         predictive_nll = None
-#         if y is not None:
-#             quadf = self._batch_quadform2(torch.inverse(sig_pred), y - mu_pred)
-#             predictive_nll = self._sig_pred_logdet(spread_fac).mean() + quadf.mean()
-        
-#         return mu_pred, sig_pred, predictive_nll
-    
-#     def _batch_quadform1(self, A, b):
-#         # Eq 8 helper
-#         #   Also equivalent to: ((b @ A) * b).sum(dim=-1)
-#         return torch.einsum('...ij,...jk,...ik->...i', b, A, b)
-    
-#     def _batch_quadform2(self, A, b):
-#         # Eq 10 helper
-#         return torch.einsum('...i,...ij,...j->...', b, A, b)
-    
-#     def _sig_pred_logdet(self, spread_fac):
-#         # Compute logdet(sig_pred)
-#         # Equivalent to [[ss.logdet() for ss in s] for s in sig_pred]
-#         return self.y_dim * (spread_fac.log() + self.log_sig_eps)
-
+        return self.blr(phi_support, y_support, phi_query, y_query)
