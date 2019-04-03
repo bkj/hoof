6d72213e09a2b827ab2b5eed685c00c9f937f9b0
Ben Johnson <bkj.322@gmail.com>
2019-04-02 20:48:11 -0400

    ...

--- experiments/simple_example.py
+++ experiments/simple_example.py
@@ -78,50 +78,6 @@ valid_history = model.do_valid(dataset=valid_dataset, **train_kwargs)
 print('final_train_loss=%f' % np.mean(train_history[-100:]), file=sys.stderr)
 print('final_valid_loss=%f' % np.mean(valid_history[-100:]), file=sys.stderr)
 
-# >>
-
-from scipy.optimize import minimize
-
-x_s, y_s, x_q, y_q, fn = valid_dataset.sample_one(support_size=20, query_size=20)
-y_s += np.random.normal(0, 1, y_s.shape) * 0.1
-y_q += np.random.normal(0, 1, y_q.shape) * 0.1
-
-model = model.eval()
-model = model.cpu()
-
-inp = list2tensors((x_s, y_s, x_q, y_q), cuda=model.is_cuda)
-init = float(model(*inp)[-1])
-print(init)
-
-inp = list2tensors((x_s, y_s, x_s, y_s), cuda=model.is_cuda)
-
-model = model.eval()
-model = model.cpu()
-
-def _target(p):
-    model.blr.sig_eps     = float(10 ** p)
-    model.blr.log_sig_eps = np.log(float(10 ** p))
-    return float(model(*inp)[-1])
-
-
-
-opt_res = minimize(_target, np.array([-1]), method='Powell', bounds=[(-10, 2)])
-
-sig_eps = float(10 ** opt_res.x)
-
-sig_eps
-
-model.blr.sig_eps     = sig_eps
-model.blr.log_sig_eps = np.log(sig_eps)
-float(model(*inp)[-1])
-
-inp = list2tensors((x_s, y_s, x_q, y_q), cuda=model.is_cuda)
-float(model(*inp)[-1])
-
-
-
-# <<
-
 # --
 # Plot example
 
@@ -134,44 +90,35 @@ np.random.seed(456)
 valid_dataset.set_seed(123)
 fig, ax = plt.subplots(3, 3)
 
-def make_target(model, inp):
-    def _target(p):
-        model.blr.sig_eps     = float(10 ** p)
-        model.blr.log_sig_eps = np.log(float(10 ** p))
-        return float(model(*inp)[-1])
-    
-    return _target
-
 
 for i in range(3):
     for j in range(3):
-        x_s, y_s, _, _, fn = valid_dataset.sample_one(support_size=20, query_size=0)
+        x_s, y_s, _, _, fn = valid_dataset.sample_one(support_size=5, query_size=0)
         x_grid = np.linspace(*valid_dataset.x_range, 1000).reshape(-1, 1)
         y_grid = fn(x_grid)
         
         y_s += np.random.normal(0, 1, y_s.shape) * 0.1
         
-        # >>
-        # Tune sig_eps
+        # # >>
+        # # Tune sig_eps
         
-        model.blr.sig_eps     = 1
-        model.blr.log_sig_eps = 0
+        # model.blr.sig_eps     = 1
+        # model.blr.log_sig_eps = 0
         
-        tune_sig_eps = True
-        if tune_sig_eps:
-            inp_target = list2tensors((x_s, y_s, x_s, y_s), cuda=model.is_cuda)
-            _target    = make_target(model, inp_target)
-            opt_res    = minimize(_target, np.array([-1]), method='Powell', bounds=[(-10, 2)])
-            sig_eps    = float(10 ** opt_res.x)
-            model.blr.sig_eps     = sig_eps
-            model.blr.log_sig_eps = np.log(sig_eps)
+        # tune_sig_eps = True
+        # if tune_sig_eps:
+        #     inp_target = list2tensors((x_s, y_s, x_s, y_s), cuda=model.is_cuda)
+        #     _target    = make_target(model, inp_target)
+        #     opt_res    = minimize(_target, np.array([-1]), method='Powell', bounds=[(-10, 2)])
+        #     sig_eps    = float(10 ** opt_res.x)
+        #     model.blr.sig_eps     = sig_eps
+        #     model.blr.log_sig_eps = np.log(sig_eps)
         
-        # <<
+        # # <<
         
         inp = list2tensors((x_s, y_s, x_grid, y_grid), cuda=model.is_cuda)
         mu, sig, nll = model(*inp)
-        print(float(nll))
-        mu, sig = tensors2list((mu, sig), squeeze=True)
+        mu, sig = tensors2list((mu, model.blr.spread_fac), squeeze=True)
         
         _ = ax[i,j].plot(x_grid, y_grid, c='black', alpha=0.25)
         _ = ax[i,j].plot(x_grid, mu)
